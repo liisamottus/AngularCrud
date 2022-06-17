07838e57fa60edeb81c498314015d9fac9c50439
LIISA <liisammottus@gmail.com>
2022-06-17 17:28:57 +0300

    landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,32 @@ import { AppComponent } from './app.component';
 import { ListEmployeesComponent } from './employees/list-employees.component';
 import { CreateEmployeeComponent } from './employees/create-employee.component';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './employees/landing-page/landing-page.component';
+
+const appRoutes: Routes = [
+  {path: 'list', component: ListEmployeesComponent},
+  {path: 'create', component: CreateEmployeeComponent},
+  {path: 'landing-page', component: LandingPageComponent},
+  {path: '', redirectTo:'/list', pathMatch:'full'}];
 
 @NgModule({
   declarations: [
     AppComponent,
     ListEmployeesComponent,
-    CreateEmployeeComponent
+    CreateEmployeeComponent,
+    LandingPageComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
